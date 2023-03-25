71f12f71cc99b3db44f485a90c1606b6006470e9
sunny <joe-918@163.com>
2023-03-25 14:37:25 +0800

    无重复的最长子序列方法二

--- samplecode/samplecode/main.cpp
+++ samplecode/samplecode/main.cpp
@@ -7,8 +7,10 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 
 int lengthOfLongestSubstring(std::string s);
+int lengthOfLongestSubstring2(std::string s);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -18,10 +20,12 @@ int main(int argc, const char * argv[]) {
         "aabcdba",
         "abcde",
         "eabcae",
+        "abcbd",
     };
 
     for (auto s : strs) {
         std::cout <<"maxlenth of: "<<s<<": "<<lengthOfLongestSubstring(s)<<std::endl;
+        std::cout <<"maxlenth of: "<<s<<": "<<lengthOfLongestSubstring2(s)<<std::endl;
     }
 
     return 0;
@@ -50,7 +54,7 @@ int lengthOfLongestSubstring(std::string s) {
             left = std::max(left, charset[c] + 1);
         }
         charset[c] = i;
-        //i - left + 1 是因为left-i之间都是无重复子串，需要包含left位置的元素	
+        //i - left + 1 是因为left-i之间都是无重复子串，需要包含left位置的元素
         maxLen = std::max(maxLen, i - left + 1);
     }
 
@@ -59,4 +63,27 @@ int lengthOfLongestSubstring(std::string s) {
      * 所以必须用left来记录当前遍历过程中的无重复字串
      */
     return maxLen;
+}
+
+int lengthOfLongestSubstring2(std::string s) {
+    std::unordered_set<char> set;
+    int left = 0, right = 0, maxLength = 0;
+    /**
+     * 方法二，维护一个无序集合（unordered_set）来检测是否有重复字符。
+     * left和right两个指针分别指向当前检测到的子串的左端点和右端点，然后不断地扩大右端点，同时判断是否有重复字符，如果有则缩小左端点，直到没有重复字符为止。
+     * 在这个过程中，记录下最长的子串的长度并返回即可。
+     */
+    while (right < s.size()) {
+        if (set.find(s[right]) == set.end()) {
+            set.insert(s[right]);
+            maxLength = std::max(maxLength, right - left + 1);
+            right++;
+        } else {
+            //如果有重复字符，则窗口左端前进一位，右端不变，进行下一轮检查，直到窗口中无重复字符为止
+            //比如abcc,检测到第二个c时就把窗口中的abc全清掉
+            set.erase(s[left]);
+            left++;
+        }
+    }
+    return maxLength;
 }
